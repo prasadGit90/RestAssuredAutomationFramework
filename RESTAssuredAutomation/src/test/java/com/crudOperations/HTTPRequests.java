c86ced538f1d5f9e7f33371b1a080ef4a76adf96
prasadGit90 <kondetip90@gmail.com>
2024-03-26 21:05:53 +0000

    added another crud operations example

---
+++ RESTAssuredAutomation/src/test/java/com/crudOperations/HTTPRequests.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package com.crudOperations;
+
+/**
+ * @author prasadkondeti
+ *
+ */
+
+
+/*
+ prerequisits 
+ Given()
+ content type,set cookies,add auth,add param,set headers info etc
+ 
+ When()
+ 
+ get ,post ,put ,delete,
+ 
+ then()
+ 
+ Validation status code ,extract response, extract headers and coockies and response body
+ 
+ */
+import static  io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+
+import javax.management.InvalidApplicationException;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class HTTPRequests {
+	
+	
+	//global variable for update and delete ID 
+	
+	int id;
+
+	@Test(priority = 1)
+	public void getSingleUser() 
+	
+	{
+		given()
+		
+		.when()
+			.get("https://reqres.in/api/users?page=2")
+		
+		.then()
+			.statusCode(200)
+			.body("page",equalTo(2))
+			.log().all();
+			
+		
+	}
+	
+	@Test(priority = 2)
+	public void createUser() {
+		
+		HashMap data= new HashMap();
+		
+		data.put("name","Prasad");
+		data.put("job","Software");
+		
+		id=given()
+			.contentType("Application/json")
+			.body(data)
+		.when()
+			.post("https://reqres.in/api/users")
+			.jsonPath().getInt("id");
+		
+		//.then()
+		//	.statusCode(201)
+		//	.log().all();
+		
+	}
+	
+	@Test(priority = 3,dependsOnMethods = {"createUser"})
+	public void updateUser() {
+
+		HashMap data= new HashMap();
+		data.put("name","James");
+		data.put("job","AutomationEngineer");
+		
+		given()
+			.contentType("Application/json")
+			.body(data)
+		.when()
+			.put("https://reqres.in/api/users/"+id)
+			
+		
+		.then()
+			.statusCode(200)
+			.log().all();
+		
+		
+	}
+	@Test(priority = 4)
+	public void deleteUser() {
+		
+		
+		given()
+		
+		.when()
+			.delete("https://reqres.in/api/users/"+id)
+		.then()
+		
+			.statusCode(204)
+			.log().all();
+		
+		
+	}
+	
+	
+
+}
